331266c4631e1f0be18f2c231323276eb29f7bc8
Pridexs <alehstk@gmail.com>
2016-08-28 20:05:02 -0300

    Montando base para trab complementar 3

---
+++ CGR/trab_complementar_3/include/PTimer.h
@@ -0,0 +1,37 @@
+#ifndef TRAB3_PTIMER
+#define TRAB3_PTIMER
+
+#include <SDL2/SDL.h>
+
+class PTimer
+{
+    public:
+		//Initializes variables
+		PTimer();
+
+		//The various clock actions
+		void start();
+		void stop();
+		void pause();
+		void unpause();
+
+		//Gets the timer's time
+		Uint32 getTicks();
+
+		//Checks the status of the timer
+		bool isStarted();
+		bool isPaused();
+
+    private:
+		//The clock time when the timer started
+		Uint32 mStartTicks;
+
+		//The ticks stored when the timer was paused
+		Uint32 mPausedTicks;
+
+		//The timer status
+		bool mPaused;
+		bool mStarted;
+};
+
+#endif
