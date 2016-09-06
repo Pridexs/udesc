ecc723fd3904b6137fead8ac7e02a8f07362b8aa
Pridexs <alehstk@gmail.com>
2016-09-05 21:47:11 -0300

    Settando trabalho de CAL

---
+++ cal/trabalho1/include/heapsort.h
@@ -0,0 +1,10 @@
+// Alexandre Maros & Nadyan S. Pscheidt
+// CAL - Trabalho 1
+
+#ifndef HEAPSORT_H
+#define HEAPSORT_H
+
+void heapify(int arr[], int n, int i);
+void heapSort(int arr[], int n);
+
+#endif
