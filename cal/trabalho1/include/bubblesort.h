fae543974cb7447a9f231851ef27ccc2dce5f6e4
Nadyan <nadyan_sp@hotmail.com>
2016-09-21 00:02:36 -0300

    bah

---
+++ cal/trabalho1/include/bubblesort.h
@@ -0,0 +1,9 @@
+// Alexandre Maros & Nadyan S. Pscheidt
+// CAL - Trabalho 1
+
+#ifndef BUBBLE_H
+#define BUBBLE_H
+
+void bubbleSort(int arr[], int n);
+
+#endif
---
+++ cal/trabalho1/include/insertsort.h
@@ -0,0 +1,9 @@
+// Alexandre Maros & Nadyan S. Pscheidt
+// CAL - Trabalho 1
+
+#ifndef INSERT_H
+#define INSERT_H
+
+void insertSort(int arr[], int n);
+
+#endif
