7bb2d1881ff1a573ad220be56d8e2472d4c52a96
Pridexs <alehstk@gmail.com>
2016-11-12 12:48:16 -0200

    1034 done

--- cal/uri/1034.cpp
+++ cal/uri/1034.cpp
@@ -1,37 +1,53 @@
 #include <cstdio>
 #include <iostream>
 #include <vector>
+#include <cmath>
+#include <algorithm>
 
 using namespace std;
 
 int blocos[26];
-vector<int> minBlocos;
-int t, n, m, b;
+int minBlocos[1000001];
+int t, n, m, b, i, j;
 
-int min(z) {
-    int menor = 0;
-    for (int i = 0; i < n; i++) {
-        if ( 1+minBlocos[z-1]  )
+int comp(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int min() {
+    int bloco;
+
+    if (m % blocos[n-1] == 0) {
+        return m/blocos[n-1];
     }
+
+    minBlocos[0] = 0;
+    for (i = 1; i <= m; i++) {
+        minBlocos[i] = 1+minBlocos[i-1];
+        for (j = 0; j < n; j++) {
+            if (i - blocos[j] >= 0) {
+                minBlocos[i] = min(minBlocos[i], 1+minBlocos[i-blocos[j]]);
+            } else {
+                break;
+            }
+        }
+    }
+
+    return minBlocos[m];
 }
 
 int main() {
     scanf("%d", &t);
 
     while(t--) {
-        scanf("%d", &n);
+        scanf("%d %d", &n, &m);
 
         for (int i = 0; i < n; i++) {
             scanf("%d", &blocos[i]);
         }
 
-        qsort(minblocos
-
-        minBlocos.clear();
-        minBlocos.push_back(0);
-        for (int i = 1; i < m; i++) {
-            minBlocos.push_bacK(min(i));
-        }
+        qsort(blocos, n, sizeof(int), comp);
+        printf("%d\n", min());
     }
 
 }
