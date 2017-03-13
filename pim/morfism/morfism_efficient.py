e1487f2238bea35d0d0692ee17130ac6ae0548d6
alepmaros <alehstk@gmail.com>
2017-03-13 08:02:24 -0300

    Dont need to get array for every loop

--- pim/morfism/morfism_efficient.py
+++ pim/morfism/morfism_efficient.py
@@ -15,11 +15,12 @@ import numpy as np
 img1 = Image.open('img1.jpg')
 img2 = Image.open('img2.jpg')
 
+M1 = np.array(img1)
+M2 = np.array(img2)
+
 for u in range(0,11):
     print("Generating image " + str(u) + "...")
     alpha = u / 10
-    M1 = np.array(img1)
-    M2 = np.array(img2)
     M3 = (1-alpha) * M1 + alpha * M2          
     
     img_morphed = Image.fromarray(np.uint8(M3))
