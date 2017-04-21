1151a9a71993885b2c0e8f9de126cd20c03f6dc5
Mateus Boiani <mateuz.boiani@gmail.com>
2017-04-21 12:08:38 -0300

    Função para Equalizar o Histograma - OK

--- pim/tarefa2/hist_eq.py
+++ pim/tarefa2/hist_eq.py
@@ -6,11 +6,38 @@ from numpy.random import randn
 from PIL import Image
 
 def calculateHistogram(img):
-    hist = [0] * 255
+    hist = [0] * 256
     data = list(img.getdata())
     for p in data:
         hist[p[0]] += 1
     return hist
+
+def histogramEqualization(img, hist):
+    height, width = img.size
+    #quantidade de pixels da img
+    size = float(height * width)
+    p_f = [0] * 256
+    g_k = [0] * 256
+    where_to_put = [0] * 256
+    accumulate_sum = 0.0
+    for i in range(0, len(hist)):
+        p_f[i] = hist[i]/size
+        accumulate_sum += p_f[i]
+        g_k[i] = accumulate_sum
+        where_to_put[i] = int(g_k[i] * 255 + 0.5)
+
+    #print(where_to_put)
+    new_hist = [0] * 256
+    for i in range(0, len(hist)):
+        new_hist[ where_to_put[i] ] +=  hist[i]
+
+    print("Antes de equalizar: ")
+    print(hist)
+    print("Depois de equalizar: ")
+    print(new_hist)
+
+    return new_hist
+
 
 # Inicio
 if (len(sys.argv) != 2):
@@ -27,4 +54,4 @@ plt.clf()
 
 # Obtendo o Histogram manualmente
 histogram = calculateHistogram(img)
-
+histogram_ = histogramEqualization(img, histogram)
